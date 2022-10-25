c61247bccad90d3320142e61a50a393661b90a5c
Marcelo Rebeque <marcelo.rebeque@gmail.com>
2022-10-25 12:24:34 -0300

    Ajustes iniciais.

---
+++ target/generated-sources/annotations/br/gov/rj/fazenda/email/corp/mapper/ClienteMapperImpl.java
@@ -0,0 +1,82 @@
+package br.gov.rj.fazenda.email.corp.mapper;
+
+import br.gov.rj.fazenda.email.corp.dto.ClienteEntradaDTO;
+import br.gov.rj.fazenda.email.corp.dto.ClienteSaidaDTO;
+import br.gov.rj.fazenda.email.corp.dto.ClienteSaidaDTO.ClienteSaidaDTOBuilder;
+import br.gov.rj.fazenda.email.corp.entity.Cliente;
+import br.gov.rj.fazenda.email.corp.entity.Cliente.ClienteBuilder;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-10-25T12:23:52-0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.14 (Red Hat, Inc.)"
+)
+@Component
+public class ClienteMapperImpl implements ClienteMapper {
+
+    @Override
+    public ClienteSaidaDTO toDTO(Cliente cliente) {
+        if ( cliente == null ) {
+            return null;
+        }
+
+        ClienteSaidaDTOBuilder clienteSaidaDTO = ClienteSaidaDTO.builder();
+
+        clienteSaidaDTO.id( cliente.getId() );
+        clienteSaidaDTO.nome( cliente.getNome() );
+        clienteSaidaDTO.razaoSocial( cliente.getRazaoSocial() );
+        clienteSaidaDTO.cpf( cliente.getCpf() );
+        clienteSaidaDTO.cnpj( cliente.getCnpj() );
+        clienteSaidaDTO.rg( cliente.getRg() );
+        clienteSaidaDTO.dataNascimento( cliente.getDataNascimento() );
+        clienteSaidaDTO.email( cliente.getEmail() );
+        clienteSaidaDTO.telefone( cliente.getTelefone() );
+        clienteSaidaDTO.celular( cliente.getCelular() );
+        clienteSaidaDTO.rua( cliente.getRua() );
+        clienteSaidaDTO.numero( cliente.getNumero() );
+        clienteSaidaDTO.complemento( cliente.getComplemento() );
+        clienteSaidaDTO.bairro( cliente.getBairro() );
+        clienteSaidaDTO.cep( cliente.getCep() );
+        clienteSaidaDTO.numeroCNH( cliente.getNumeroCNH() );
+        clienteSaidaDTO.numeroRegistroCNH( cliente.getNumeroRegistroCNH() );
+        clienteSaidaDTO.validadeCNH( cliente.getValidadeCNH() );
+        clienteSaidaDTO.senha( cliente.getSenha() );
+        clienteSaidaDTO.observacao( cliente.getObservacao() );
+
+        return clienteSaidaDTO.build();
+    }
+
+    @Override
+    public Cliente toEntity(ClienteEntradaDTO clienteEntradaDTO) {
+        if ( clienteEntradaDTO == null ) {
+            return null;
+        }
+
+        ClienteBuilder cliente = Cliente.builder();
+
+        cliente.id( clienteEntradaDTO.getId() );
+        cliente.nome( clienteEntradaDTO.getNome() );
+        cliente.razaoSocial( clienteEntradaDTO.getRazaoSocial() );
+        cliente.cpf( clienteEntradaDTO.getCpf() );
+        cliente.cnpj( clienteEntradaDTO.getCnpj() );
+        cliente.rg( clienteEntradaDTO.getRg() );
+        cliente.dataNascimento( clienteEntradaDTO.getDataNascimento() );
+        cliente.email( clienteEntradaDTO.getEmail() );
+        cliente.telefone( clienteEntradaDTO.getTelefone() );
+        cliente.celular( clienteEntradaDTO.getCelular() );
+        cliente.rua( clienteEntradaDTO.getRua() );
+        cliente.numero( clienteEntradaDTO.getNumero() );
+        cliente.complemento( clienteEntradaDTO.getComplemento() );
+        cliente.bairro( clienteEntradaDTO.getBairro() );
+        cliente.cep( clienteEntradaDTO.getCep() );
+        cliente.numeroCNH( clienteEntradaDTO.getNumeroCNH() );
+        cliente.numeroRegistroCNH( clienteEntradaDTO.getNumeroRegistroCNH() );
+        cliente.validadeCNH( clienteEntradaDTO.getValidadeCNH() );
+        cliente.senha( clienteEntradaDTO.getSenha() );
+        cliente.observacao( clienteEntradaDTO.getObservacao() );
+
+        return cliente.build();
+    }
+}
---
+++ target/generated-sources/annotations/br/gov/rj/fazenda/email/corp/mapper/FornecedorMapperImpl.java
@@ -0,0 +1,66 @@
+package br.gov.rj.fazenda.email.corp.mapper;
+
+import br.gov.rj.fazenda.email.corp.dto.FornecedorEntradaDTO;
+import br.gov.rj.fazenda.email.corp.dto.FornecedorSaidaDTO;
+import br.gov.rj.fazenda.email.corp.dto.FornecedorSaidaDTO.FornecedorSaidaDTOBuilder;
+import br.gov.rj.fazenda.email.corp.entity.Fornecedor;
+import br.gov.rj.fazenda.email.corp.entity.Fornecedor.FornecedorBuilder;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-10-25T12:23:52-0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.14 (Red Hat, Inc.)"
+)
+@Component
+public class FornecedorMapperImpl implements FornecedorMapper {
+
+    @Override
+    public FornecedorSaidaDTO toDTO(Fornecedor fornecedor) {
+        if ( fornecedor == null ) {
+            return null;
+        }
+
+        FornecedorSaidaDTOBuilder fornecedorSaidaDTO = FornecedorSaidaDTO.builder();
+
+        fornecedorSaidaDTO.id( fornecedor.getId() );
+        fornecedorSaidaDTO.nomeFantasia( fornecedor.getNomeFantasia() );
+        fornecedorSaidaDTO.razaoSocial( fornecedor.getRazaoSocial() );
+        fornecedorSaidaDTO.cpf( fornecedor.getCpf() );
+        fornecedorSaidaDTO.cnpj( fornecedor.getCnpj() );
+        fornecedorSaidaDTO.inscricaoMunicipal( fornecedor.getInscricaoMunicipal() );
+        fornecedorSaidaDTO.inscricaoEstadual( fornecedor.getInscricaoEstadual() );
+        fornecedorSaidaDTO.rg( fornecedor.getRg() );
+
+        return fornecedorSaidaDTO.build();
+    }
+
+    @Override
+    public Fornecedor toEntity(FornecedorEntradaDTO entradaDTO) {
+        if ( entradaDTO == null ) {
+            return null;
+        }
+
+        FornecedorBuilder fornecedor = Fornecedor.builder();
+
+        fornecedor.id( entradaDTO.getId() );
+        fornecedor.nomeFantasia( entradaDTO.getNomeFantasia() );
+        fornecedor.razaoSocial( entradaDTO.getRazaoSocial() );
+        fornecedor.cpf( entradaDTO.getCpf() );
+        fornecedor.cnpj( entradaDTO.getCnpj() );
+        fornecedor.inscricaoMunicipal( entradaDTO.getInscricaoMunicipal() );
+        fornecedor.inscricaoEstadual( entradaDTO.getInscricaoEstadual() );
+        fornecedor.rg( entradaDTO.getRg() );
+        fornecedor.email( entradaDTO.getEmail() );
+        fornecedor.telefone( entradaDTO.getTelefone() );
+        fornecedor.celular( entradaDTO.getCelular() );
+        fornecedor.rua( entradaDTO.getRua() );
+        fornecedor.numero( entradaDTO.getNumero() );
+        fornecedor.complemento( entradaDTO.getComplemento() );
+        fornecedor.bairro( entradaDTO.getBairro() );
+        fornecedor.cep( entradaDTO.getCep() );
+
+        return fornecedor.build();
+    }
+}
